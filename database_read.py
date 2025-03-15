dd6fe65a4b389bc5cc9f726e68e6af7f3f95715e
Kuma Kuma <103328204+Flo314rian@users.noreply.github.com>
2025-03-15 04:45:22 +0100

    Datenbank mit backup und GUI übertragung

--- database_read.py
+++ database_read.py
@@ -1,17 +1,32 @@
-import random
-from mysql.connector import errorcode
-from sqlalchemy import create_engine,text
-import serial,time
+from sqlalchemy import create_engine, Column,Integer, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import serial, time, random, os, subprocess
+from datetime import datetime
 from flask import Flask, jsonify
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
+
+user = 'root'
+password = 'Etraxx_25'
+host = 'localhost'
+database = 'test_01'
+port = "3306"  
+path = '/Users/imagine_losing/Desktop/Backup_files'
+
+connection_url= f"mysql+mysqlconnector://{user}:{password}@{host}/{database}"
+engine = create_engine(connection_url, echo=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
    
-
 
 app = Flask(__name__)
 
-@app.route('/incoming_data', methods=['GET'])
 def get_data(): 
+
+    current_time = int(time.time())
+
     speed = random.randint(0,115)
     accel = random.randint(0,100)
     brake = random.randint(0,100) 
@@ -33,6 +48,7 @@ def get_data():
 
     x_gyro = random.uniform(-1, 1)
     y_gyro = random.uniform(-1, 1)
+    z_gyro = random.uniform(-1, 1)
 
     battery_status = random.uniform(0,100)
     
@@ -44,12 +60,14 @@ def get_data():
     error_soc = random.randint(0,1)
     error_BSPD = random.randint(0,1)
     error_current = random.randint(0,1)
-    error_votlage = random.randint(0,1)
+    error_voltage = random.randint(0,1)
     error_inverter_undervoltage = random.randint(0,1)
     error_test = random.randint(0,1)
 
 
-    return jsonify({
+    return {
+        "current_time": current_time,
+
         "speed": speed,
         "ACCEL":accel,
         "BRAKE": brake,
@@ -80,158 +98,241 @@ def get_data():
             "soc":error_soc,
             "error_current":error_current,
             "BSPD": error_BSPD,
-            "error_voltage":error_votlage,
-            "error_undervotlage":error_inverter_undervoltage,
+            "error_voltage":error_voltage,
+            "error_undervoltage":error_inverter_undervoltage,
             "error_test":error_test
         }
-    })
+    }
+
+def give_data_to_database():
+        data = get_data()
+        save_to_database(data)
 
 
 
-user = 'root'
-password = 'Etraxx_25'
-host = 'localhost'
-database = 'test_01'
+def save_to_database(data):
+    session = Session()
 
-connection_url= f"mysql+mysqlconnector://{user}:{password}@{host}/{database}"
-engine = create_engine(connection_url, echo=True)
+    try:
 
+        apps_data = Apps(
+            time = data["current_time"],
+            apps_accel = data["ACCEL"],
+            apps_brake = data["BRAKE"]
+        )
+        session.add(apps_data)
 
+        # Tabelle WheelSpeed
+        wheel_data = WheelSpeed(
+            time = data["current_time"],
+            speed = data["speed"]
+        )
+        session.add(wheel_data)
 
-TABLES = {}
-TABLES['APPS'] = (
-    "CREATE TABLE IF NOT EXISTS `APPS` ("
-    "id INT PRIMARY KEY AUTO_INCREMENT,"
-    "  `time` int,"
-    "  `APPS ACCEL` int,"
-    "  `APPS BRAKE` int"
-    ") ENGINE=InnoDB"
-)
+        # Tabelle Soc
+        soc_data = Soc(
+            time =data["current_time"],
+            soc1 = data["soc_1"],
+            soc2 = data["soc_2"]
+        )
+        session.add(soc_data)
 
-TABLES['wheel_speed']=(
-     "CREATE TABLE IF NOT EXISTS `wheel_speed` ("
-    "id INT PRIMARY KEY AUTO_INCREMENT,"
-    "  `time` int,"
-    "  `speed` int"
-    ") ENGINE=InnoDB"
+        # Tabelle SteeringAngle
+        steering_data = SteeringAngle(
+            time = data["current_time"],
+            steering_angle = random.randint(0, 720)  # Beispielwert
+        )
+        session.add(steering_data)
 
-)
+        # Tabelle CurrentSensor
+        current_sensor_data = CurrentSensor(
+            time = data["current_time"],
+            current_sensor = data["current"]  
+        )
+        session.add(current_sensor_data)
 
-TABLES['soc'] = (
-     "CREATE TABLE IF NOT EXISTS `soc` ("
-    "id INT PRIMARY KEY AUTO_INCREMENT,"
-    "  `time` int,"
-    "  `SOC 1` int,"
-    "  `SOC 2` int"
-    ") ENGINE=InnoDB"
-)
+        # Tabelle Suspension
+        suspension_data = Suspension(
+            time = data["current_time"],
+            suspension_fr = data["FR"],
+            suspension_fl = data["FL"],
+            suspension_rr = data["RR"],
+            suspension_rl = data["RL"]
+        )
+        session.add(suspension_data)
 
+        # Tabelle Gyro
+        gyro_data = Gyro(
+            time = data["current_time"],
+            sbg_roll = data["x_gyro"],
+            sbg_pitch = data["y_gyro"],
+            #sbg_yaw = data["z_gyro"]               # Muss ich noch hinzufügen, grad kein bock
+        )
+        session.add(gyro_data)
 
-#TABLES['brake_pressure'] = (
-#     "CREATE TABLE `brake_pressure` ("
-#    "id INT PRIMARY KEY AUTO_INCREMENT,"
-#    "`time` int,"
-#    "`values` int"
-#    ") ENGINE=InnoDB"
-#
-#
-#TABLES['brake_throttle'] = (
-#   "CREATE TABLE `brake_throttle` ("
-#  "id INT PRIMARY KEY AUTO_INCREMENT,"
-#  "  `time` int,"
-#  "  `values` int"
-#  ") ENGINE=InnoDB"
-#
-#
-TABLES['steering_angle'] = (
-     "CREATE TABLE IF NOT EXISTS `steering_angle` ("
-    "id INT PRIMARY KEY AUTO_INCREMENT,"
-    "  `time` int,"
-    "  `Steering angle` int"
-    ") ENGINE=InnoDB"
-)
+        # Tabelle Temperature
+        temperature_data = Temperature(
+            temperature_motor = data["temp_motor"],
+            temperature_inverter = data["temp_inverter"],
+            temperature_battery = data["temp_battery"]
+        )
+        session.add(temperature_data)
 
-TABLES['current_sensor'] = (
-     "CREATE TABLE IF NOT EXISTS `current_sensor` ("
-    "id INT PRIMARY KEY AUTO_INCREMENT,"
-    "  `time` int,"
-    "  `current sensor` int"
-    ") ENGINE=InnoDB"
-)
+        #Tabelle Inverter
+        inverter_data = Inverter(
+            time = data["current_time"],
+            input_inverter_power = data["power"]
+        )
+        session.add(inverter_data)
 
-TABLES['suspension'] = (
-     "CREATE TABLE IF NOT EXISTS `suspension` ("
-    "id INT PRIMARY KEY AUTO_INCREMENT,"
-    "  `time` int,"
-    "  `suspension FR` int,"
-    "  `suspension FL` int,"
-    "  `suspension RR` int,"
-    "  `suspension RL` int"
-    ") ENGINE=InnoDB"
-)
+        # Tabelle Erros
+        error_data= Errors(
 
-TABLES['gyro'] = (
-     "CREATE TABLE IF NOT EXISTS `gyro` ("
-    "id INT PRIMARY KEY AUTO_INCREMENT,"
-    "  `time` int,"
-    "  `SBG-roll` int,"
-    "  `SBG-pitch` int,"
-    "  `SBG-yaw` int"
-    ") ENGINE=InnoDB"
-)
+            error_undervoltage=data["error"]["error_undervoltage"],  
+            error_voltage=data["error"]["error_voltage"],
+            error_bspd=data["error"]["BSPD"],
+            error_soc=data["error"]["soc"],
+            error_temperature_motor=data["error"]["motor"],
+            error_temperature_battery=data["error"]["battery"],
+            error_temperature_inverter=data["error"]["inverter"]
+        )
 
-TABLES['Temperature'] = (
-     "CREATE TABLE IF NOT EXISTS `Temperature` ("
-    "id INT PRIMARY KEY AUTO_INCREMENT,"
-    "  `TEMPERATURE Motor` int,"
-    "  `TEMPERATURE Inverter` int,"
-    "  `TEMPERATURE Battery` int"
-    ") ENGINE=InnoDB"
-)
+        session.add(error_data)
 
-#TABLES['torque_slipcontroll'] = (
-#     "CREATE TABLE `torque_slipcontroll` ("
-#    "id INT PRIMARY KEY AUTO_INCREMENT,"
-#    "  `time` int,"
-#    "  `values` int"
-#    ") ENGINE=InnoDB"
-#)
+        session.commit()
 
-TABLES['Inverter'] = (
-     "CREATE TABLE IF NOT EXISTS `Inverter` ("
-    "id INT PRIMARY KEY AUTO_INCREMENT,"
-    "  `time` int,"
-    "  `Input/Inverter power` int"
-    ") ENGINE=InnoDB"
-)
+    except Exception as e:
+        session.rollback()  
+        print(f"Datenbankfehler: {e}")
 
-TABLES['errors'] = (
-     "CREATE TABLE IF NOT EXISTS `errors` ("
-    "id INT PRIMARY KEY AUTO_INCREMENT,"
-    "  `time` int,"
-    "  `error undervotlage` int,"
-    "  `error current` int,"
-    "  `error voltage` int,"
-    "  `error BSPD` int,"
-    "  `error soc` int,"
-    "  `error Temperature motor` int,"
-    "  `error Temperature battery` int,"
-    "  `error Temperature inverter` int"
-    ") ENGINE=InnoDB"
-)
-
-# erstellt die tables
-with engine.connect() as connection:
-    for table_name in TABLES:
-        table_description = TABLES[table_name]
-        connection.execute(text(table_description))
+    finally:
+        session.close() 
 
 
 
 
+class Apps(Base):
+    __tablename__ = 'APPS'  
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time = Column(Integer)
+    apps_accel = Column("APPS ACCEL", Integer) 
+    apps_brake = Column("APPS BRAKE", Integer)  
+
+
+class WheelSpeed(Base):
+    __tablename__ = 'wheel_speed'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time = Column(Integer)
+    speed = Column(Integer)
+
+class Soc(Base):
+    __tablename__ = 'soc'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time = Column(Integer)
+    soc1 = Column("SOC 1", Integer)
+    soc2 = Column("SOC 2", Integer)
+
+class SteeringAngle(Base):
+    __tablename__ = 'steering_angle'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time = Column(Integer)
+    steering_angle = Column("Steering angle", Integer)
+
+class CurrentSensor(Base):
+    __tablename__ = 'current_sensor'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time = Column(Integer)
+    current_sensor = Column("current sensor", Integer)
+
+class Suspension(Base):
+    __tablename__ = 'suspension'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time = Column(Integer)
+    suspension_fr = Column("suspension FR", Integer)
+    suspension_fl = Column("suspension FL", Integer)
+    suspension_rr = Column("suspension RR", Integer)
+    suspension_rl = Column("suspension RL", Integer)
+
+class Gyro(Base):
+    __tablename__ = 'gyro'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time = Column(Integer)
+    sbg_roll = Column("SBG-roll", Float)  
+    sbg_pitch = Column("SBG-pitch", Float)
+    sbg_yaw = Column("SBG-yaw", Float)
+
+class Temperature(Base):
+    __tablename__ = 'Temperature'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    temperature_motor = Column("TEMPERATURE Motor", Integer)
+    temperature_inverter = Column("TEMPERATURE Inverter", Integer)
+    temperature_battery = Column("TEMPERATURE Battery", Integer)
+
+
+class Inverter(Base):
+    __tablename__ = 'Inverter'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time = Column(Integer)
+    input_inverter_power = Column("Input/Inverter power", Integer)
+
+class Errors(Base):
+    __tablename__ = 'errors'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    time = Column(Integer)
+    error_undervoltage = Column("error undervotlage", Integer)
+    error_current = Column("error current", Integer)
+    error_voltage = Column("error voltage", Integer)
+    error_bspd = Column("error BSPD", Integer)
+    error_soc = Column("error soc", Integer)
+    error_temperature_motor = Column("error Temperature motor", Integer)
+    error_temperature_battery = Column("error Temperature battery", Integer)
+    error_temperature_inverter = Column("error Temperature inverter", Integer)
+
+
+#startet seperat  give_data_to_database mit interval
+scheduler = BackgroundScheduler()
+scheduler.add_job(give_data_to_database, 'interval', seconds = 2)
+scheduler.start()
+
+
+
+
+def backup():
+
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    backup_file = os.path.join(path, f"backup_{current_time}.sql")
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    command = (
+        f"mysqldump --no-tablespaces "
+        f"--host={host} --port={port} --user={user} --password={password} {database} "
+        f"> \"{backup_file}\" 2>/dev/null"
+    )
+
+    try:
+        subprocess.run(command, shell=True)
+        print(f"Backup erfolgreich: {backup_file}")
+    except subprocess.CalledProcessError as e:
+        print(f"Backup fehlgeschlagen: {e}")
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(backup, 'interval', seconds = 30)
+scheduler.start()
+
+
+
+@app.route('/incoming_data', methods=['GET'])
+def transfer_data():
+    data = get_data()
+    
+    return jsonify(data)
 
 
 if __name__ == '__main__':
-    app.run(host = '127.0.0.1',port = 8024)
+    Base.metadata.create_all(engine)
+    app.run(host = '127.0.0.1',port = 8024, debug = True)
 
 
