a22b5460a6e4e730b524d4e0dcc25fb7fbab73d2
Kuma Kuma <103328204+Flo314rian@users.noreply.github.com>
2025-04-03 23:35:34 +0200

    neuer stand: einlesen läuft

--- serial_reader02.py
+++ serial_reader02.py
@@ -4,61 +4,61 @@ import serial, time,re
 baudrate = 115200
 serial_port = "COM3"  
 
-ser = serial.Serial(serial_port, baudrate, timeout=0.3)  
+#ser = serial.Serial(serial_port, baudrate, timeout=0.3)  
 
 
 ID_LENGHTS = {
-    '1':2,
-    '2':3,
-    '3':2,
-    '4':1,
-    '5':2
+    '1':2,      # erhält werte von 
+    '2':4,
+    '3':6,
+    '4':10,
+    'A':5
 }
 
-def process_data():           # bekommt die daten aus seperating_data
+def process_data(pieces_of_hexa,id_str):           # bekommt die daten aus seperating_data
 
-    print()
+    print(f"ID: {id_str}, processed piece: {pieces_of_hexa}")
 
 
 
 def seperating_data(message):
 
     parts = message.split(',')
-
     if len(parts) < 4:
-        print("zu wenig Nachrichten")
-        continue
+        
+        raise ValueError("Die Nachricht entspricht nicht dem erwarteten Format")
 
     hexa_message = parts[2] 
     ids = parts[3]
 
-    i = 0
-    while i <len(ids):
-        id_data = ids[i]
-
+    
+    j = 0
+    for id_data in ids:
         if id_data in ID_LENGHTS:
-            lenght_in_bytes = ID_LENGHTS(id_data)
+            lenght_in_bytes = ID_LENGHTS[id_data]
             lenght_in_hexa = lenght_in_bytes * 2
-            #abhängig wie groß habe ihc jetzt die größe rus jeweiliger id erstmal
 
+            
+            seperated_info = hexa_message[j:j+lenght_in_hexa]
+            process_data(seperated_info, id_data)
 
-
-
-    process_data()
-    print("star")
+            j+=lenght_in_hexa
+    print("Fertig mit der erhaltenen Nachricht")
+        
 
 
 
 def main():
 
     #kommt noch ein loop
-    message = ser.readline('utf-8', errors='ignore').strip()
+    #message = ser.readline('utf-8', errors='ignore').strip()
 
-    seperating_data(message)
-
+    test_message = "2,n,FA20C832A34F7BCDE83FA83C23BCD321DE21985F3421AB234C2321DB8896AFF12,123A4"
+    seperating_data(test_message)
     print("hier startet was")
 
 
-        
+if __name__ == "__main__":
+    main()
         
 
