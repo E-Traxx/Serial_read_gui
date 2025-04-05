2874b1d003589c3a43adfd8df6848e225d96a0d8
Kuma Kuma <103328204+Flo314rian@users.noreply.github.com>
2025-04-05 20:40:22 +0200

    message process quasi fertig Flask server in bearbeitung und Datenbank fehlt

--- serial_reader02.py
+++ serial_reader02.py
@@ -1,4 +1,6 @@
 import serial, time,re 
+from flask import Flask, jsonify
+from datetime import datetime
 
 
 baudrate = 115200
@@ -8,16 +10,69 @@ serial_port = "COM3"
 
 
 ID_LENGHTS = {
-    '1':2,      # erhält werte von 
-    '2':4,
-    '3':6,
-    '4':10,
-    'A':5
+    '1': {'total_length': 12,
+        'infos': [
+            (8, "Leistung"),
+            (4, "error_power")
+        ]},
+    '2':{ 'total_length': 24,
+        'infos': [
+            (16, "error"),
+            (8, "OINK OINK")
+        ]},
+    '3': {'total_length': 16,
+        'infos': [
+            (12, "i_dont_know"),
+            (4, "voltage ")
+        ]},
+    '4': {'total_length': 12,
+        'infos': [
+            (4, "notio"),
+            (8, "hallo")
+        ]},
+    'A': {'total_length': 20,
+        'infos': [
+            (12, "time"),
+            (8, "car")
+        ]}
 }
+
+def flask_server(data):
+
+    current_time = int(datetime.now().strftime('%H%M%S'))
+    
+
+
+    return
+
+
+
+
+
 
 def process_data(pieces_of_hexa,id_str):           # bekommt die daten aus seperating_data
 
-    print(f"ID: {id_str}, processed piece: {pieces_of_hexa}")
+    temp = pieces_of_hexa   #einzelne nachricht
+    details = ID_LENGHTS.get(id_str)    #komplette nachrichten
+    fields = details.get('infos')   #infos 
+    
+    result = {}
+
+    index = 0
+    for info_lenght, value_description in fields:
+        message_hexa_lenght = info_lenght // 4            #info_lenght in bit: 4 bit = 1 hexa, 1 byte = 8 bit = 2 hexa
+        each_value_in_hexa = temp[index: index + message_hexa_lenght]
+        index+=message_hexa_lenght
+
+        each_value = int(each_value_in_hexa,16)
+
+        print(f"ID: {id_str} - {value_description}: {each_value} (Hex: {each_value_in_hexa})")
+
+        result[value_description] = each_value 
+
+    flask_server(result)
+    print(result)
+
 
 
 
@@ -32,17 +87,16 @@ def seperating_data(message):
     ids = parts[3]
 
     
-    j = 0
+    index = 0
     for id_data in ids:
         if id_data in ID_LENGHTS:
-            lenght_in_bytes = ID_LENGHTS[id_data]
-            lenght_in_hexa = lenght_in_bytes * 2
-
+            message = ID_LENGHTS[id_data]
+            total_length_hexa = message['total_length'] // 4
             
-            seperated_info = hexa_message[j:j+lenght_in_hexa]
+            seperated_info = hexa_message[index:index + total_length_hexa]
             process_data(seperated_info, id_data)
 
-            j+=lenght_in_hexa
+            index+=total_length_hexa
     print("Fertig mit der erhaltenen Nachricht")
         
 
@@ -53,7 +107,7 @@ def main():
     #kommt noch ein loop
     #message = ser.readline('utf-8', errors='ignore').strip()
 
-    test_message = "2,n,FA20C832A34F7BCDE83FA83C23BCD321DE21985F3421AB234C2321DB8896AFF12,123A4"
+    test_message = "2,n,FA110832A34F7BCDE83FA83C23BCD321DE21985F3421AB234C2321DB8896AFF12312,123A4"
     seperating_data(test_message)
     print("hier startet was")
 
